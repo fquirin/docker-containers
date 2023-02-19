bd02ebff7537230ef5197de9cef0b5488912cea2
Florian Quirin <info@bytemind.de>
2023-02-19 23:52:34 +0100

    fixed a bug in Coqui build script

--- SEPIA/Coqui-TTS/build_container.sh
+++ SEPIA/Coqui-TTS/build_container.sh
@@ -7,7 +7,7 @@ elif [ -n "$(uname -m | grep armv7l)" ]; then
 	echo "Sorry, but ARM 32bit (armv7l) builds are currently not supported."
 	exit 1
 else
-	PLATFORM="amd64" .
+	PLATFORM="amd64"
 fi
 echo "Building Coqui-TTS Docker container: $IMAGE_NAME:$PLATFORM"
 sudo docker build --no-cache -t "$IMAGE_NAME:$PLATFORM" .
